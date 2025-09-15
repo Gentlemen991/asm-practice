2941f2acacae2fb6661be6d4c705fb2d8ee69b41
Gentlemen991 <trefrex30@gmail.com>
2025-09-15 22:42:22 +0300

    PR1

---
+++ homework/hw1.asm
@@ -0,0 +1,19 @@
+section .data
+msg:    db  "Hello, world!", 10   ; строка + перевод строки
+len     equ $ - msg               ; длина строки
+
+section .text
+global _start
+
+_start:
+    ; write(1, msg, len)
+    mov     rax, 1        ; syscall: sys_write
+    mov     rdi, 1        ; fd = 1 (stdout)
+    lea     rsi, [rel msg]; pointer to msg
+    mov     rdx, len      ; length
+    syscall
+
+    ; exit(0)
+    mov     rax, 60       ; syscall: sys_exit
+    xor     rdi, rdi      ; status = 0
+    syscall
